583c27c735111babbafcdd3d3369c09a2d928fc7
lenka-zelenka <elenazologina@gmail.com>
2018-04-05 00:03:03 +0300

    Tas2 HW 6

--- lections/lection_6_functions/homework/main.js
+++ lections/lection_6_functions/homework/main.js
@@ -1,5 +1,5 @@
 // # Практика
-
+separator = '<br />* * * * * * * * * *<br />'
 // 1) Написать функцию заполнения двумерного массива. Имя произвольное.
 
 function randomFillArray (array){
@@ -18,14 +18,40 @@ function randomFillArray (array){
 n=4;
 m=5;
 myArray = new Array(n);
-for(i = 0; i< myArray.length; i++){
+for(var i = 0; i< myArray.length; i++){
     myArray[i] = new Array(m);
 }
-
+document.write('До заполнения myArray',myArray, separator);
 randomFillArray(myArray);
+document.write('Посе заполнения myArray', myArray, separator);
+
+
 
 // 2) написать функцию, которая примет как аргумент(параметр) два массива и сравнить суммы всех ЧИСЛОВЫХ элементов. Тот массив, сумма которого большая - должен вернутся функцией.
+A1  = [0,2,3,3,4,5,5,5,5,1,1,1,1,1,1,1,1,1,10000];
+A2 = [3,4,5,5,6,7,6,6,5];
 
+function arraySum (arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++ ) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+function compareArraySum (arr1, arr2){
+    if (arraySum(arr1) > arraySum(arr2) ) {
+        console.log('arr1 > arr2')
+        return arr1;
+    } else if (arraySum(arr1) < arraySum(arr2)) {
+        console.log('arr2 > arr1 ')        
+        return arr2;
+    } else {
+        console.log('arr1 == arr2');
+    }
+}
+
+compareArraySum(A1, A2)
 
 // # Лекция
 
