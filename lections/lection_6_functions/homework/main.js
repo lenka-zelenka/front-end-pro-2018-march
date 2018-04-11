326c49b6c6eb1dd48f2ad143c6a19dc2ee70a3de
lenka-zelenka <elenazologina@gmail.com>
2018-04-11 23:25:52 +0300

    fetch after lection 7

--- lections/lection_6_functions/homework/main.js
+++ lections/lection_6_functions/homework/main.js
@@ -21,21 +21,6 @@ myArray = new Array(n);
 for(var i = 0; i< myArray.length; i++){
     myArray[i] = new Array(m);
 }
-<<<<<<< HEAD
-document.write('До заполнения myArray',myArray, separator);
-randomFillArray(myArray);
-document.write('Посе заполнения myArray', myArray, separator);
-
-
-
-// 2) написать функцию, которая примет как аргумент(параметр) два массива и сравнить суммы всех ЧИСЛОВЫХ элементов. Тот массив, сумма которого большая - должен вернутся функцией.
-A1  = [0,2,3,3,4,5,5,5,5,1,1,1,1,1,1,1,1,1,10000];
-A2 = [3,4,5,5,6,7,6,6,5];
-
-function arraySum (arr) {
-    var sum = 0;
-    for (var i = 0; i < arr.length; i++ ) {
-=======
 document.write(' <br />Массив после создания <br />', myArray)
 randomFillArray(myArray);
 document.write(' <br />Массив после заполнения <br />', myArray)
@@ -52,27 +37,11 @@ document.write(' <br />Массив 1<br />', A2)
 function sumArray (arr) {
     var sum = 0;
     for(i = 0; i < arr.length; i++) {
->>>>>>> cc37160c8198d9f64eedd990d7c8a97c536a7274
         sum += arr[i];
     }
     return sum;
 }
 
-<<<<<<< HEAD
-function compareArraySum (arr1, arr2){
-    if (arraySum(arr1) > arraySum(arr2) ) {
-        console.log('arr1 > arr2')
-        return arr1;
-    } else if (arraySum(arr1) < arraySum(arr2)) {
-        console.log('arr2 > arr1 ')        
-        return arr2;
-    } else {
-        console.log('arr1 == arr2');
-    }
-}
-
-compareArraySum(A1, A2)
-=======
 function compareArrays (arr1, arr2) {
     if (sumArray(arr1) > sumArray(arr2)) {
         document.write('arr1 > arr2');
@@ -88,7 +57,6 @@ document.write(' <br />Сумма массива 1 =' + sumArray(A1) + '<br />')
 document.write(' <br />Сумма массива 2 =' + sumArray(A2) + '<br />')
 
 compareArrays(A1, A2)
->>>>>>> cc37160c8198d9f64eedd990d7c8a97c536a7274
 
 // # Лекция
 
