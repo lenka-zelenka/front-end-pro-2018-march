c84792928c79edf59f14eaf5fe85a41cb8048b04
lenka-zelenka <elenazologina@gmail.com>
2018-05-09 14:05:41 +0300

    HW 11 done

--- lections/lection_11_OOP/homework/main.js
+++ lections/lection_11_OOP/homework/main.js
@@ -24,24 +24,51 @@ Calculator1.prototype.render = function(){
 }
 Calculator1.prototype.clear = function() {
     this.x = 0;
-}
 
+}
+// 2 уровень
 function Calculator2(x) {
     this.x = x;
 }
 
 Calculator2.prototype = Object.create(Calculator1.prototype);
-
 Calculator2.prototype.setValue = function(val) {
     this.x = val;
 }
 
 Calculator2.prototype.Sum = function(a,b) {
-    this.setValue(a+b);
+    this.setValue(this.Square(a+b) );
+}
+Calculator2.prototype.Div = function(a,b) {
+    this.setValue(this.Square(a/b));
+}
+Calculator2.prototype.Mul = function(a,b) {
+    this.setValue(this.Square(a*b));
+}
+Calculator2.prototype.Square = function(square) {
+    return square * square;
 }
 
-var p = new Calculator1(0);
-var p2 = new Calculator2(0);
+// 3 уровень
+function Calculator3(x) {
+    arr = [];
+    arr.length = 200;
+}
+Calculator3.prototype = Object.create(Calculator2.prototype);
 
-// var p = new Calculator1(25);
-// var p2 = new Calculator2();
+Calculator3.prototype.reInit = function() {
+    for (var i = 0; i < this.arr.length; i++) {
+        this.arr[i] = Math.floor(Math.random() * 50);
+        document.write(this.arr[i] + ' ');
+    }
+}
+
+// 4 уровень
+function Calculator4(x) {}
+Calculator4.prototype = Object.create(Calculator3.prototype);
+
+var p4 = new Calculator4();
+
+p4.arr = [1,2,3,4];
+p4.reInit()
+p4.Sum(2,5)
