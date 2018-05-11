e323f562dff26df6b52a41f857fbe624739f1ab0
elenazologina <elenazologina@gmail.com>
2018-05-11 18:00:47 +0300

    HW_14 fix bug with 50%

--- lections/lection_14_DOM_events/homework/main.js
+++ lections/lection_14_DOM_events/homework/main.js
@@ -1,16 +1,21 @@
 window.onload = function(){
     var range = document.getElementById('range'),
         number = document.getElementById('number');
-        
-        
+    
+    document.querySelector('#comission-value').innerHTML =  getComissionAmount(number.value) + "%";
+    document.querySelector('#range-value').innerHTML =  number.value + "%"
     range.addEventListener('input', function(){
         drawDiagram (this.value);
-        setValue (number, this.value)         
-    } )
+        setValue (number, this.value);
+        document.querySelector('#comission-value').innerHTML =  getComissionAmount(this.value) + "%";
+        document.querySelector('#range-value').innerHTML =  this.value + "%"     
+    } );
     number.addEventListener('input', function(){
         drawDiagram (this.value);
-        setValue (range, this.value) 
-    } )
+        setValue (range, this.value);
+        document.querySelector('#comission-value').innerHTML =  getComissionAmount(this.value) + "%";
+        document.querySelector('#range-value').innerHTML =  this.value + "%";
+    } );
 }
 function setValue (input, value) {
     input.value = value;
@@ -29,7 +34,7 @@ function getComissionAmount (range){
         return 2;
     }else if (range >= 20 && range < 50) {
         return 4;
-    } else if (range >+ 50 && range < 75) {
+    } else if (range >= 50 && range < 75) {
         return 6;
     } else if (range >= 75 && range <= 100) {
         return 8
